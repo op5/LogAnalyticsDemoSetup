0cfaf53a5cccccceb50eef97bdd86442199b2645
rqadmin <rqadmin@dayton.d.rktxvs.com>
2018-08-03 16:22:25 -0500

    Adding functions to main shell script.
    Adding .gitignore and sedtest.txt

--- loganalyticsdemosetup.sh
+++ loganalyticsdemosetup.sh
@@ -2,6 +2,7 @@
 
 ACCOUNT=''
 PASSWORD=''
+BASHRC=./sedtest.sh
 
 function createuser {
 	read -rp "User account: " account
@@ -14,11 +15,19 @@ function setpassword {
 	 passwd $ACCOUNT 
 }
 
+function clean_bashrc {
+	sed -i '' -n '/$0/d'$BASHRC
+}
+
+function finish {
+	#shutdown -r now
+	printf "Rebooting VM. (Not really)\n"
+}
+
 printf "Log Analytics Demo Setup.\n"
 
 createuser
 setpassword
-
-
-
+clean_bashrc
+finish
 
