5095c4b5e35c45bbfb9a1717aad02e8a4e2c0886
joncavanaugh <joncavanaugh@gmail.com>
2018-08-06 09:34:18 -0500

    Switch to nmcli
    Changed from manual method of setting up network to nmcli and rounded off questions/responses as well as commented each function and any decision.

--- setup_network.sh
+++ setup_network.sh
@@ -1,44 +1,52 @@
 #!/bin/bash
 
-read -p "Enter Your Name: "  username
-echo "Welcome $username!"
-
-Need to know the interface Name
-
-Need to set the following in the interface file
-Need to store the answers in variables
-
-function SET_NETWORK()
+function SETUP_LA()
 {
-network_inf="/etc/sysconfig/network-scripts/ifcfg-enp0s3"
+echo "AWESOME DHCP!!!!"
+}
 
-read -p "Enter the IP address to use for this server: " ipaddr
+# Setup the network settings for when not using DHCP (Advanced)
+# We provide network list, ask some questions so we can set this automatically
+function SETUP_NETWORK()
+{
+echo "Below you'll see the the intefaces found on your machine, for the next step you'll type in the name of the one you want to use for the Log Analytics server."
+ip ntable | grep dev | sort | uniq | sed -e 's/^.*dev //;/^lo/d'
+
+read -p "Enter the inteface name to use for this Log Analytics server: " inf_name
+read -p "Enter the IP address to use for this server and CIDR notation (example 10.10.0.100/24): " ipaddr
 read -p "Enter the network mask (i.e. 255.255.255.0):" netmask
 read -p "Enter the gateway for this network:" gateway
 read -p "Enter the IP address of your first DNS server:" dns1
 read -p "Enter the IP address of your second DNS server:" dns2
 
-sed -i "
-/IPADDR=/c\IPADDR=${ipaddr};
-/NETMASK=/c\NETMASK=${netmask};
-/GATEWAY=/c\GATEWAY=${gateway};
-/DNS1=/c\DNS1=${dns1};
-/DNS2=/c\DNS2=${dns2};
-" $network_inf
-}
-echo $(SET_NETWORK);
+# Share the collected network information with the nmcli
+nmcli con add con-name "$inf_name" ifname $inf_name type ethernet ip4 $ipaddr gw4 $netmask
+nmcli con mod "LogAnalytics-Network" ipv4.dns "$dns1,$dns2"
+nmcli con up "LogAnalytics-Network" ifname $inf_name
 
+# Confirm the settings which the user entered by outputting ip addr and asking
+# Next steps undefined byt can replace echo
+echo -e "Do these network settings look correct?"
 
-echo "Your network configuration is: /r IPa"
-echo "Welcome $username!"
-
+ip addr | grep -A 1 -B 1 $inf_name
 
 read -r -p "Are you sure? [y/N] " response
 case "$response" in
     [yY][eE][sS]|[yY])
-        do_something
+        echo AWESOME NETWORK SETUP COMPLETE!!
         ;;
     *)
-        do_something_else
+        SETUP_LA
         ;;
 esac
+}
+
+# Initial prompt to ask if user would like to use DHCP or setup the static network settings
+read -r -p "Would you like to use DHCP (1) or Static IP **Advanced** (2)? " response
+case "$response" in
+    1)
+        SETUP_LA
+        ;;
+    2)
+        SETUP_NETWORK
+        ;;
