c2e390e3aef718095aa41f87745c6c4751d7dd2f
Ryan Quinn <rquinn@op5.com>
2018-08-09 17:49:25 -0400

    Checkpoint WIP#1
    Network works,
    Repos functions work,
    Setting user/pass works,
    Cleanup in progress.

--- loganalyticsdemosetup.sh
+++ loganalyticsdemosetup.sh
@@ -3,6 +3,107 @@
 ACCOUNT=''
 PASSWORD=''
 BASHRC=./sedtest.sh
+NETWORK_GATEWAY=''
+NETWORK_ACCESS=0
+REPO_BASE="https://github.com"
+REPO_PATH="op5/LogAnalyticsDemoSetup"
+REPO_ACCESS=0
+REPO_ON_DISK="$HOME/LogAnalyticsDemoSetup"
+SCRIPT_NAME="loganalyticsdemosetup.sh"
+
+function SET_DHCP() {
+	echo "AWESOME DHCP!!!!"
+}
+
+# Setup the network settings for when not using DHCP (Advanced)
+# We provide network list, ask some questions so we can set this automatically
+function SETUP_NETWORK() {
+	echo "Below you'll see the the intefaces found on your machine, for the next step you'll type in the name of the one you want to use for the Log Analytics server."
+	nmcli -p connection show
+	
+	read -p "Enter the inteface name to use for this Log Analytics server: " inf_name
+	read -p "Enter the IPv4 address and subnet for this server in CIDR notation (ex: 10.1.0.100/24): " ipaddr
+	read -p "Enter the gateway for this network: " gateway
+	read -p "Enter the IPv4 address of your first DNS server: " dns1
+	read -p "Enter the IPv4 address of your second DNS server: " dns2
+	
+	# Saving the gateway to make sure we have connectivity.
+	NETWORK_GATEWAY=$gateway
+
+	# Share the collected network information with the nmcli
+	printf "\n"
+	printf "Making sure $inf_name starts on boot.\n"
+	nmcli connection modify "$inf_name" connection.autoconnect yes
+	printf "Setting IPv4 address on $inf_name to $ipaddr.\n"
+	nmcli connection modify "$inf_name" ipv4.address $ipaddr
+	printf "Setting IPv4 gateway on $inf_name to $gateway.\n"
+	nmcli connection modify "$inf_name" ipv4.gateway $gateway
+	printf "Setting IPv4 DNS on $inf_name to $dns1 $dns2.\n"
+	nmcli connection modify "$inf_name" ipv4.dns "$dns1 $dns2"
+	printf "Setting $inf_name static.\n"
+	nmcli connection modify "$inf_name" ipv4.method manual
+
+	# Confirm the settings which the user entered by outputting ip addr and asking
+	# Next steps undefined byt can replace echo
+	echo -e "Do these network settings look correct?"
+	
+	printf "\n$inf_name info:\n"
+	ip -h addr show $inf_name
+	printf "\nRoute info:\n"
+	ip -h route
+	
+	read -r -p "Are you sure? [Yes/No/Dhcp] " response
+	case "$response" in
+	    [yY][eE][sS]|[yY])
+		printf "Bouncing connection for settings to take effect.\n"
+		nmcli connection up "$inf_name"
+	        printf "AWESOME NETWORK SETUP COMPLETE!!\n"
+	        ;;
+	    [dD][hH][cC][pP]|[dD])
+		SET_DHCP
+		;;
+	    *)
+	        SETUP_NETWORK
+	        ;;
+	esac
+}
+
+function SETUP_NETWORK_CHOICE() {
+	# Initial prompt to ask if user would like to use DHCP or setup the static network settings
+	read -r -p "Would you like to use DHCP (1) or Static IP **Advanced** (2)? " response
+	case "$response" in
+	    1)
+	        SET_DHCP
+	        ;;
+	    2)
+	        SETUP_NETWORK
+	        ;;
+	esac
+}
+
+function network_ping_gw {
+	ping -Ac3 $NETWORK_GATEWAY printf "\n" 
+	if [[ $? -ne 0 ]] 
+	then
+		printf "Unable to contact gateway.\n"
+		printf "Continuing without network access.\n"
+		NETWORK_ACCESS=1
+	fi
+}
+
+function network_resolve_repo_hostname {
+	curl --connect-timeout 20 -s $REPO_BASE/$REPO_PATH -o /dev/null
+	if [[ $? -ne 0 ]]
+	then
+		printf "Unable to resolve $REPO_BASE\n"
+		REPO_ACCESS=1
+	fi
+}
+
+function repo_update {
+	cd $REPO_ON_DISK
+	git pull
+}
 
 function createuser {
 	read -rp "User account: " account
@@ -15,8 +116,28 @@ function setpassword {
 	 passwd $ACCOUNT 
 }
 
+function NewUser {
+	createuser
+	setpassword
+}
+
+function setrootpass {
+	printf "Setting root password then.\n"
+	ACCOUNT="root"
+	setpassword
+
+}
+
+function RootSwitch {
+	read -p "Would you like to create a local user? [Yes/No]: " choice 
+	case "$choice" in
+		Yes|yes|y|Y ) NewUser;;
+		No|no|n|N ) setrootpass;;
+	esac
+}
+
 function clean_bashrc {
-	sed -i '' -n '/$0/d'$BASHRC
+	sed -i '' -n "/$SCRIPT_NAME/d" $BASHRC
 }
 
 function finish {
@@ -26,8 +147,25 @@ function finish {
 
 printf "Log Analytics Demo Setup.\n"
 
-createuser
-setpassword
+#SETUP_NETWORK_CHOICE
+#network_ping_gw
+if [[ $NETWORK_ACCESS -eq 0 ]]
+then
+	printf "Check to see if repo host is resolvable.\n"
+	#network_resolve_repo_hostname
+else
+	printf "Skipping resolving repo host since network access is in an unknown state.\n"
+fi
+
+if [[ $NETWORK_ACCESS -eq 0 && $REPO_ACCESS -eq 0 ]]
+then
+	printf "Getting any repo updates.\n"
+	#repo_update
+else
+	printf "Skipping pulling repo updates.\n"
+fi
+
+#RootSwitch
 clean_bashrc
-finish
+#finish
 
