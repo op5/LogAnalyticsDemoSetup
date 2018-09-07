95d28d87969c75b5a8b9829a6cbf2e789e471d37
Ken Dobbins <kdobbins@op5.com>
2018-09-07 05:06:29 +0200

    changed to absolute paths.

--- generalcommands.sh
+++ generalcommands.sh
@@ -1,3 +1,2 @@
-./filebeat -e -c filebeat.yml
-./flog/flog -f apache_combined -n 20 >> /root/LogAnalyticsDemoSetup/apache_combined.log
-
+/root/LogAnalyticsDemoSetup/filebeat/filebeat -e -c /root/LogAnalyticsDemoSetup/filebeat/filebeat.yml
+/root/LogAnalyticsDemoSetup/flog/flog -f apache_combined -n 20 >> /root/LogAnalyticsDemoSetup/apache_combined.log
